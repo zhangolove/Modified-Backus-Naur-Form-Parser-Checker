062a4b4220dfd54f8788b3973cad45f3953525b6
zhangolove <cl46@rice.edu>
2016-10-06 11:51:42 -0500

    refator the code

--- mbnf_checker.py
+++ mbnf_checker.py
@@ -4,24 +4,87 @@ import requests, re, os
 
 
 
-def compare_answer(l_student, l_web):
-	set_names = ['FIRST SET', 'FOLLOW SET', 'FIRST+ SET']
-	for i, (ds, dw) in enumerate(zip(l_student, l_web)):
-		if len(ds) < len(dw):
-			print "ERROR: the number of elements in your %s is less than that of the web" % set_names[i]
-		for symbol, set_ in ds.items():
-			if symbol not in dw:
-				print "warning: the %s of the symbol (%s) in your solution is not presented in the web solution" %(set_names[i], symbol)
-			else:
-				if set_ != dw[symbol]:
-					#The predict set do not have epsilon while the first+ set do
-					if not (i == 2 and set_ - dw[symbol] == {'EPSILON'}):
-						print "Error: for the %s of the symbol (%s), your solution differ from the web" %(set_names[i], symbol)
-						print "	Yours", set_
-						print "	Web", dw[symbol]
-						print
+def mbnf_2_ebnf(fh):
+	"""
+	Input: a file handle that contains a language of mbnf grammar
+	Output: a string of equivalent language in ebnf form
+	"""
+	mbnf = fh.read()
+	symbol_reg = r'[\n\s]*([a-zA-Z0-9]+)[\s\n]*'
+	rhs_reg = r'[\n\s]*([a-zA-Z0-9]+)([a-zA-Z0-9 ]*)\n*'
+	ir = []
+	mbnf = re.sub(re.compile("//.*?\n" ) ,"" ,mbnf)
+	plist = mbnf.split(';')
+	for p in plist:
+		split_p = p.split(':')
+		if len(split_p) == 2:
+			left, right = p.split(':')
+			rhss = right.split('|')
+			lhs = re.match(symbol_reg, left).group(1)
+			rhs_rep = []
+			for rhs in rhss:
+				match = re.match(rhs_reg, rhs)
+				rhs_rep.append(match.group(1) + match.group(2))
+			new_rep = "%s -> %s" %(lhs, ' | '.join(rhs_rep))
+			ir.append(new_rep)
+	ebnf = re.sub(r'epsilon', r'EPSILON', '\n'.join(ir), flags=re.IGNORECASE)
+	return ebnf
 
-def file_to_sets(fh):
+def fetch_answer_from_web(ebnf):
+	"""
+	Input: a string in ebnf grammar
+	Output: an html file returned by the hackingoff website that contains the answer
+	"""
+	payload = {'grammar' : ebnf}
+	with requests.session() as s:
+		checker_url = 'http://hackingoff.com/compilers/predict-first-follow-set'
+		response_post = s.post(checker_url, data=payload)
+		return response_post.text
+
+def parse_answer_from_web(data):
+	"""
+	Parse the answer from the html file
+	Output: A list of dictionary [first, follow, first+]
+			each dictionary is in the form of {symbol: set([symbol...])}
+	"""
+
+	soup = BeautifulSoup(data, "html.parser")
+	tables = soup.find_all(class_='set-table')
+	set_list = []
+	translate_table = {u'$' : '<EOF>', u'\u03b5': 'EPSILON'}
+	for table in tables:
+		rows = table.find_all('tr')[1:]
+		symbol2set = {}
+		for row in rows:
+			tds = map(lambda x: x.text, row.find_all('td'))
+			symbol = tds[0] if tds[0] not in translate_table else translate_table[tds[0]]
+			s_set = {w if w not in translate_table else translate_table[w] for w in tds[-1].split(', ')}
+			symbol2set[symbol] = s_set
+		set_list.append(symbol2set) 
+	return set_list
+
+
+
+def parse_answer_from_student(fh):
+	"""
+	fh should be written in the following format:
+
+		First set
+			symbol_name_a: {symbol_name_1, symbol_name_2....symbol_name_n}
+			symbol_name_b: {symbol_name_1, symbol_name_2....symbol_name_n}
+
+		Follow set
+			symbol_name_a: {symbol_name_1, symbol_name_2....symbol_name_n}
+			symbol_name_b: {symbol_name_1, symbol_name_2....symbol_name_n}
+
+		First+ set
+			symbol_name_a: {symbol_name_1, symbol_name_2....symbol_name_n}
+			symbol_name_b: {symbol_name_1, symbol_name_2....symbol_name_n}
+
+	The keywords are "First set", "Follow set", and "First+ set", the program use these
+	keywords to determine which set you are referring to. The sections must be in the above
+	order and separated by a blank line.
+    """
 	patterns = [r'first set', r'follow set', r'first\+ set']
 	set_list = []
 	for line in fh:
@@ -32,8 +95,7 @@ def file_to_sets(fh):
 			
 
 def match_element(iter):
-	#should look like    symbol : {symbol, symbol}
-	#symbol list must be separated by', '
+	# helper function to parse all the element for a set
 	pattern = r'\s*([a-zA-Z0-9<>]+)\s*:\s*{([a-zA-Z0-9,<>\s]+)}'
 	symbol2set = {}
 	while True:
@@ -51,88 +113,9 @@ def match_element(iter):
 	return symbol2set
 
 
-def read(fname):
-	try:
-		fh = open(fname)
-		return fh
-	except IOError as e:
-		raise SystemExit("I/O error({0}): {1}".format(e.errno, e.strerror))
-
-def parse_answer(data):
-	#print data
-	soup = BeautifulSoup(data, "html.parser")
-	tables = soup.find_all(class_='set-table')
-	set_list = []
-	translate_table = {u'$' : '<EOF>', u'\u03b5': 'EPSILON'}
-	for table in tables:
-		rows = table.find_all('tr')[1:]
-		symbol2set = {}
-		for row in rows:
-			tds = map(lambda x: x.text, row.find_all('td'))
-			symbol = tds[0] if tds[0] not in translate_table else translate_table[tds[0]]
-			s_set = {w if w not in translate_table else translate_table[w] for w in tds[-1].split(', ')}
-			symbol2set[symbol] = s_set
-		set_list.append(symbol2set) 
-		# print
-		#print symbol2set
-	return set_list
-
-def fetch_answer(ebnf):
-	payload = {'grammar' : ebnf}
-	with requests.session() as s:
-		checker_url = 'http://hackingoff.com/compilers/predict-first-follow-set'
-		response_post = s.post(checker_url, data=payload)
-		return response_post.text
-
-# def find_files():
-# 	import fnmatch
-# 	import os
-# 	for file in os.listdir('.'):
-# 		if fnmatch.fnmatch(file, '*.txt'):
-
-
-def mbnf_2_ebnf(fh):
-	#ebnf = mbnf.replace(':', '->').replace(';','').replace('epsilon','EPSILON')
-	mbnf = fh.read()
-	symbol_reg = r'[\n\s]*([a-zA-Z0-9]+)[\s\n]*'
-	rhs_reg = r'[\n\s]*([a-zA-Z0-9]+)([a-zA-Z0-9 ]*)\n*'
-	ir = []
-	mbnf = re.sub(re.compile("//.*?\n" ) ,"" ,mbnf)
-	plist = mbnf.split(';')
-	for p in plist:
-		split_p = p.split(':')
-		if len(split_p) == 2:
-			left, right = p.split(':')
-			rhss = right.split('|')
-			#print "left: ", left
-			lhs = re.match(symbol_reg, left).group(1)
-			rhs_rep = []
-			for rhs in rhss:
-				#print "original rhs", rhs
-				match = re.match(rhs_reg, rhs)
-				rhs_rep.append(match.group(1) + match.group(2))
-			new_rep = "%s -> %s" %(lhs, ' | '.join(rhs_rep))
-			ir.append(new_rep)
-	ebnf = re.sub('epsilon', 'EPSILON', '\n'.join(ir), re.IGNORECASE)
-	return ebnf
-
-
-def check_answer(folder_path, student_folder):
-	for f in find_files(folder_path):
-		print "now Comparing file " + f
-		answer_web = parse_answer(fetch_answer(mbnf_2_ebnf(read(f))))
-		student_path = os.path.join(student_folder, os.path.basename(f))
-		answer_student = file_to_sets(read(student_path))
-		compare_answer(answer_student, answer_web)
-		
-		print "=========================="
-
-
-
-
 
 def find_files(path):
-	
+	# returns file ending with '.ll1' or '.nonll1' in a folder
 	matches = []
 	for root, dirnames, filenames in os.walk(path):
 		for basename in filenames:
@@ -140,15 +123,69 @@ def find_files(path):
 				yield os.path.join(root, basename)
 
 
+def read(fname):
+	# return an opened file
+	try:
+		fh = open(fname)
+		return fh
+	except IOError as e:
+		raise SystemExit("I/O error({0}): {1}".format(e.errno, e.strerror))
+
+def compare_answer(l_student, l_web):
+	"""
+	This function will compare two lists of dictionary
+	l_student = [d_first, d_follow, d_first_plus]
+	d_first = {symbol_name: set of string}
+	"""
+	set_names = ['FIRST SET', 'FOLLOW SET', 'FIRST+ SET']
+	ret = True
+	for i, (ds, dw) in enumerate(zip(l_student, l_web)):
+		if len(ds) < len(dw):
+			print "ERROR: the number of elements in your %s is less than that of the web" % set_names[i]
+			ret = False
+		for symbol, set_ in ds.items():
+			if symbol not in dw:
+				print "warning: the %s of the symbol (%s) in your solution is not presented in the web solution" %(set_names[i], symbol)
+			else:
+				if set_ != dw[symbol]:
+					#The predict set do not have epsilon while the first+ set do
+					#if not (i == 2 and set_ - dw[symbol] == {'EPSILON'}):
+					#The web solution will use empty string to represent '<EOF>' for the goal symbol
+					if i != 2 and not (set_ == {'<EOF>'} and dw[symbol] == {u''}) :
+						print "Error: for the %s of the symbol (%s), your solution differ from the web" %(set_names[i], symbol)
+						print "	Yours", set_
+						print "	Web", dw[symbol]
+						print
+						ret = False
+	return ret
 
 
+def check_answer(grammar_path, student_folder):
+	"""
+	This function will check the first, follow sets for files in grammar_path against 
+	the answer in 'http://hackingoff.com/compilers/predict-first-follow-set'
+	Input:
+		grammar_path: a directory that contains files ending with .ll1 and .nonll1 files
+					  Each file should include a language of mbnf grammar
+		student_folder: a directory that contains files ending with .ll1 and .nonll1 files;
+						Each format of file content is specified in parse_answer_from_student; 
+						The base filenames have to be a one-to-one map from the grammar folder
+						For example, the answer for grammar_path/xx/iloc.ll1 must have the 
+						filename of student_folder/iloc.ll1.
+	"""
 
+	for f in find_files(grammar_path):
+		print "now Comparing file " + f
+		answer_web = parse_answer_from_web(fetch_answer_from_web(mbnf_2_ebnf(read(f))))
+		student_path = os.path.join(student_folder, os.path.basename(f))
+		answer_student = parse_answer_from_student(read(student_path))
+		if not compare_answer(answer_student, answer_web):
+			print "The ebnf form: "
+			print mbnf_2_ebnf(read(f))
+		f.close()
+		print "=========================="
 
 
 check_answer('./grammars/', './yaml/')
 
-
-f1 = file_to_sets(read('chris_result.txt'))
-f2 = parse_answer(fetch_answer(mbnf_2_ebnf(read('ceg-rr-simple.ll1'))))
-compare_answer(f1, f2)
 
